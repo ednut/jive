3d71c9150b0865c68565ac776e74ef3834089654
Babatunde Adegbite <babatundea@wakanow.com>
2017-05-19 07:47:27 +0100

    fixing player

--- js/main.js
+++ js/main.js
@@ -1,9 +1,53 @@
-$('.player_audio').click(function() {
-  if (this.paused == false) {
-      this.pause();
-      alert('music paused');
-  } else {
-      this.play();
-      alert('music playing');
-  }
+// ####################################################################
+
+$('.play').on('click', function() {
+    document.querySelector('.player').play();
+    $('.play').hide();
+    $('.pause').show();
 });
+$('.pause').on('click', function() {
+    document.querySelector('.player').pause();
+    $('.pause').hide();
+    $('.play').show();
+});
+$('.player').on('timeupdate', function() {
+    $('.seekbar').attr("value", this.currentTime / this.duration);
+});
+
+// ####################################################################
+
+$('.play1').on('click', function() {
+    document.querySelector('.player1').play();
+    $('.play1').hide();
+    $('.pause1').show();
+});
+
+$('.pause1').on('click', function() {
+    document.querySelector('.player1').pause();
+    $('.pause1').hide();
+    $('.play1').show();
+});
+$('.player1').on('timeupdate', function() {
+    $('.seekbar1').attr("value", this.currentTime / this.duration);
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(".imgInp").change(function(){
+    readURL(this);
+});
+
+document.getElementById("uploadBtn").onchange = function () {
+    document.getElementById("uploadFile").value = this.value;
+};
+
